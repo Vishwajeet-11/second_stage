a9053ebd4cdd8188c81aa3b75e21414e62da7cc6
Vishwajeet-11 <vishwajeetbharadiya12@gmail.com>
2024-03-04 15:23:42 +0530

    structurized the admin models with ? syntax

--- web_models/admin.js
+++ web_models/admin.js
@@ -5,24 +5,20 @@ module.exports = {
         return db.query('SELECT * FROM admin WHERE email = ?', [email])
     },
     
-    total_orders: async() => {
-        return db.query('SELECT COUNT(payment_status) FROM cart')
+    total_orders: async(payment_status, cart) => {
+        return db.query('SELECT COUNT(?) FROM ?', [payment_status, cart])
     },
 
-    seller_earning : async() => {
-        return db.query('select SUM(cart_price) FROM cart WHERE payment_status = 1')
+    seller_earning : async(cart_price, cart, payment_status) => {
+        return db.query('select SUM(?) FROM ? WHERE ? = 1',[cart_price, cart, payment_status])
     },
 
-    customers : async() => {
-        return db.query('SELECT COUNT(buyer_name) FROM tbl_buyer')
+    customers : async(buyer_name, tbl_buyer) => {
+        return db.query('SELECT COUNT(?) FROM ?',[buyer_name, tbl_buyer])
     },
 
-    fetchAllCustomers: async() => {
-        return db.query('SELECT * FROM tbl_buyer')
-    },
-
-    save: async() => {
-
+    fetchAllCustomers: async(tbl_buyer) => {
+        return db.query('SELECT * FROM ?', [tbl_buyer])
     },
 
 }
